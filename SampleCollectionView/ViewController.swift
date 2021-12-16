901b0e89e3489adc84f8d19c511cf1bf0214c86f
luigi sato <base.basketball0607@gmail.com>
2021-12-16 21:39:04 +0900

    Initial Commit

---
+++ SampleCollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleCollectionView
+//
+//  Created by 佐藤瑠偉史 on 2021/12/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
