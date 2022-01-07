9c6a228988215952b9a094ea492de99008fdf2f0
luigi sato <base.basketball0607@gmail.com>
2022-01-07 23:22:42 +0900

    cellにバッジを追加

---
+++ SampleCollectionView/Tag/View/DeleteCollectionReusableView.swift
@@ -0,0 +1,17 @@
+//
+//  DeleteCollectionReusableView.swift
+//  SampleCollectionView
+//
+//  Created by 佐藤瑠偉史 on 2022/01/07.
+//
+
+import UIKit
+
+class DeleteCollectionReusableView: UICollectionReusableView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
